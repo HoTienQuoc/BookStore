cad9328cbd529f0db603db7c49c95ba9e6274d10
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-04 20:23:06 +0700

    Update dependencies and configuration for improved functionality
    - Updated laravel/sail version to ^1.44 in composer.json
    - Updated content-hash in composer.lock
    - Changed DB_DATABASE environment variable to 'testing' in phpunit.xml
    - Added HasApiTokens trait to User model for API token management
    - Updated health check route to '/status' in app.php and added middleware for abilities
    - Configured axios to include credentials and XSRF token in bootstrap.js

--- BookStore/bootstrap/app.php
+++ BookStore/bootstrap/app.php
@@ -4,15 +4,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+ 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        // health: '/up',
+        health: '/status', 
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
